355dda924005f98a6fd63ea6289b4beed95e666c
AbyanCore <abyanstudentdev@gmail.com>
2024-11-10 20:35:03 +0700

    Refactoring all code and adding simple sdk for bridging frontend to backend

--- src/triple_entry_backend/main.mo
+++ src/triple_entry_backend/main.mo
@@ -2,4 +2,8 @@ actor {
   public shared (msg) func whoami() : async Principal {
     msg.caller
   };
+
+  public shared (msg) func greetShared() : async Text {
+    return "Hello, " # Principal.toText(msg.caller) # "!";
+  };
 };
--- src/triple_entry_frontend/src/App.jsx
+++ src/triple_entry_frontend/src/App.jsx
@@ -1,80 +1,54 @@
-import { useState, useEffect } from "react";
-import { Actor, HttpAgent } from "@dfinity/agent";
-import { AuthClient } from "@dfinity/auth-client";
-
-const webapp_id = process.env.CANISTER_ID_TRIPLE_ENTRY_BACKEND;
-
-// The interface of the whoami canister
-const webapp_idl = ({ IDL }) => {
-  return IDL.Service({ whoami: IDL.Func([], [IDL.Principal], ["query"]) });
-};
-
-export const init = ({ IDL }) => {
-  return [];
-};
+import { useEffect, useState } from "react";
+import IC from "./utils/IC";
 
 function App() {
-  const [currentIiUrl, setCurrentIiUrl] = useState("");
-  const [loginStatus, setLoginStatus] = useState("");
-  async function handleLogin() {
-    try {
-      // When the user clicks, we start the login process.
-      // First we have to create and AuthClient.
-      const authClient = await AuthClient.create();
+  const [islog, setIslog] = useState("");
+  const [greeting, setGreeting] = useState("");
 
-      // Call authClient.login(...) to login with Internet Identity. This will open a new tab
-      // with the login prompt. The code has to wait for the login process to complete.
-      // We can either use the callback functions directly or wrap them in a promise.
-      await new Promise((resolve, reject) => {
-        authClient.login({
-          identityProvider: currentIiUrl,
-          onSuccess: resolve,
-          onError: reject,
-        });
-      });
-
-      // At this point we're authenticated, and we can get the identity from the auth client:
-      const identity = authClient.getIdentity();
-      console.log(identity, "<<< IDENTITY");
-      // Using the identity obtained from the auth client, we can create an agent to interact with the IC.
-      const agent = new HttpAgent({ identity });
-      // Using the interface description of our webapp, we create an actor that we use to call the service methods.
-      const webapp = Actor.createActor(webapp_idl, {
-        agent,
-        canisterId: "bd3sg-teaaa-aaaaa-qaaba-cai",
-      });
-      // Call whoami which returns the principal (user id) of the current user.
-      console.log(webapp, "<<< WEB APP");
-      const principal = await webapp.whoami();
-      console.log(principal, "<<< PRINCIPAL!!!");
-      setLoginStatus(principal.toText());
-    } catch (e) {
-      console.log(e, "<<< ERROR");
-    }
-  }
   useEffect(() => {
-    let iiUrl;
-    if (process.env.DFX_NETWORK === "local") {
-      iiUrl = `http://${process.env.CANISTER_ID}.localhost:4943`;
-    } else if (process.env.DFX_NETWORK === "ic") {
-      iiUrl = `https://${process.env.CANISTER_ID}.ic0.app`;
-    } else {
-      iiUrl = `https://${process.env.CANISTER_ID}.dfinity.network`;
-    }
-    setCurrentIiUrl(iiUrl);
+    IC.getAuth(async (authClient) => {
+      if (await authClient.isAuthenticated()) {
+        setIslog(authClient.getIdentity().getPrincipal().toText());
+      }
+    });
   }, []);
+
   return (
     <main>
       <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <button onClick={handleLogin} type="submit">
-          Login
-        </button>
-      </div>
-      <section id="greeting">{loginStatus}</section>
-      <p>{loginStatus}</p>
+      <h1>Triple Entry Cash</h1>
+      <p>Is log: {islog}</p>
+      <button
+        onClick={() => {
+          IC.getBackend(async (be) => setGreeting(await be.greetShared()));
+        }}
+      >
+        greet
+      </button>
+      <p>{greeting}</p>
+      <button
+        onClick={() => {
+          IC.getAuth(async (authClient) => {
+            authClient.logout();
+          });
+        }}
+      >
+        Logout
+      </button>
+      <button
+        onClick={() => {
+          IC.getAuth(async (authClient) => {
+            authClient.login({
+              ...IC.defaultAuthOption,
+              onSuccess: () => {
+                setIslog(authClient.getIdentity().getPrincipal().toText());
+              },
+            });
+          });
+        }}
+      >
+        Login
+      </button>
     </main>
   );
 }
