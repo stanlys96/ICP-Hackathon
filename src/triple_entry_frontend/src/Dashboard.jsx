deb4a1d06770f67a515991ec6542d65bf638b9b8
stanlys96 <stanlyskwok@gmail.com>
2024-11-28 19:52:34 +0700

    refactor

--- src/triple_entry_frontend/src/Dashboard.jsx
+++ src/triple_entry_frontend/src/Dashboard.jsx
@@ -50,20 +50,22 @@ function Dashboard() {
         const getPrincipals = await result.getAllPrincipals();
         const uniqueData = getPrincipals.filter(
           (item, index, self) =>
-              self.findIndex(other => other?.toText() === item?.toText()) === index
+            self.findIndex((other) => other?.toText() === item?.toText()) ===
+            index
         );
         const principalsWithRoles = await Promise.all(
           uniqueData?.map(async (principal) => ({
             principal: principal,
             principalText: principal?.toText(),
-            role: Object.keys((await result.getUserRole(principal))?.[0] ?? {})?.[0],
+            role: Object.keys(
+              (await result.getUserRole(principal))?.[0] ?? {}
+            )?.[0],
           }))
         );
         setPrincipals(principalsWithRoles);
         const theRealRole = Object.keys(getRole?.[0] ?? {})?.[0];
         setTheRole(theRealRole);
         const transactions = await result.getTransactions();
-        console.log(transactions, "<<< TRANSACTIONS")
         setLoading(false);
       });
     });
@@ -71,128 +73,169 @@ function Dashboard() {
 
   return (
     <main className="the-body">
-      {loading ? <ClipLoader
+      {loading ? (
+        <ClipLoader
           color={"#FFFFFF"}
           loading={loading}
           size={50}
           aria-label="Loading Spinner"
           data-testid="loader"
-        /> : <div>
-      {mode === "dashboard" && (
-        <div className="container">
-          <button
-            onClick={async () => {
-              showModal(true);
-            }}
-            className="the-button margin-bot"
-          >
-            Logout
-          </button>
-          <p className="text-white text-center margin-bot">
-            Welcome to JASR Blockchain Dashboard
-          </p>
-          <p className="text-white text-center margin-bot">
-            Your Petty Cash Summary
-          </p>
-          <p className="text-white text-center margin-bot">Identity: {islog}</p>
-          <p className="text-white text-center margin-bot">Role: {theRole}</p>
-          <button
-            onClick={async () => {
-              setMode("transaction");
-            }}
-            className="submit-button margin-bot"
-          >
-            Add New Transaction
-          </button>
-        </div>
-      )}
-      {mode === "transaction" && (
-        <div className="transaction">
-          <p className="text-white text-center margin-bot">Transaction</p>
-          <p className="text-white text-center brown-color">
-            Enter a Blockchain or Transaction/ Origin Walet Wallet address
-          </p>
-          <p className="text-white text-center margin-bot brown-color">
-            Validating a transaction- Ensure Blockchain Address format is
-            Correct to prevent errors.
-          </p>
-          <div className="input-master">
-            <div className="input-container">
-              <label className="text-white">From Wallet Address</label>
-              <input value={islog} disabled className="disabled-inputy" />
-            </div>
-            <div className="input-container">
-              <label className="text-white">To Wallet Address</label>
-              <select value={destination} onChange={(e) => setDestination(e.target.value)} className="inputy" id="dropdown" name="options">
-                <option>Select destination</option>
-                {principals?.map(
-                  (principal) =>
-                    islog !== principal?.principalText && (
-                      <option value={principal?.principal}>{principal?.principalText?.slice(0, 25) + "..."} -{" "}{principal?.role}</option>
-                    )
-                )}
-              </select>
-            </div>
-            <div className="input-container">
-              <label className="text-white">Amount</label>
-              <input value={amount} onChange={(e) => {
-                let currentValue = e.target.value;
-                const filteredValue = currentValue
-                  .split("")
-                  .filter((char) => numberStrings.includes(char))
-                  .join("");
-                setAmount(filteredValue);
-              }} className="inputy" />
-            </div>
-            <div className="input-container">
-              <label className="text-white">Description</label>
-              <input value={description} onChange={(e) => setDescription(e.target.value)} className="inputy" />
-            </div>
-            {transactionLoading ? <div className="center-all">
-                <BeatLoader color={"#FFFFFF"} size={30} />
-              </div> : <div className="button-container">
+        />
+      ) : (
+        <div>
+          {mode === "dashboard" && (
+            <div className="container">
               <button
-                onClick={() => setMode("dashboard")}
-                className="the-button"
+                onClick={async () => {
+                  showModal(true);
+                }}
+                className="the-button margin-bot"
               >
-                Cancel
+                Logout
               </button>
-              <button onClick={() => {
-                if (!destination || !amount || !description) {
-                  return Swal.fire({
-                    title: "Fill the values!",
-                    text: "Please fill in all the inputs!",
-                    icon: "info"
-                  });
-                }
-                if (parseInt(amount) === 0) {
-                  return Swal.fire({
-                    title: "Amount info!",
-                    text: "Amount cannot be zero!",
-                    icon: "info"                    
-                  })
-                }
-                setTransactionLoading(true);
-                IC.getBackend(async(result) => {
-                  const destinationPrincipal = principals?.find((thePrincipal) => thePrincipal?.principalText === destination)?.principal;
-                  const finalResult = await result.addTransaction(currentPrincipal, destinationPrincipal, parseInt(amount), description);
-                  console.log(finalResult, "<<< FINAL RESULT");
-                  setAmount("");
-                  setDescription("");
-                  setDestination("");
-                  setTransactionLoading(false);
-                  Swal.fire({
-                    title: "Success!",
-                    text: "Successfully created the transaction!",
-                    icon: "success"
-                  });
-                })
-              }} className="submit-button">Submit</button>
-            </div>}
-          </div>
+              <p className="text-white text-center margin-bot">
+                Welcome to JASR Blockchain Dashboard
+              </p>
+              <p className="text-white text-center margin-bot">
+                Your Petty Cash Summary
+              </p>
+              <p className="text-white text-center margin-bot">
+                Identity: {islog}
+              </p>
+              <p className="text-white text-center margin-bot">
+                Role: {theRole}
+              </p>
+              <button
+                onClick={async () => {
+                  setMode("transaction");
+                }}
+                className="submit-button margin-bot"
+              >
+                Add New Transaction
+              </button>
+            </div>
+          )}
+          {mode === "transaction" && (
+            <div className="transaction">
+              <p className="text-white text-center margin-bot">Transaction</p>
+              <p className="text-white text-center brown-color">
+                Enter a Blockchain or Transaction/ Origin Walet Wallet address
+              </p>
+              <p className="text-white text-center margin-bot brown-color">
+                Validating a transaction- Ensure Blockchain Address format is
+                Correct to prevent errors.
+              </p>
+              <div className="input-master">
+                <div className="input-container">
+                  <label className="text-white">From Wallet Address</label>
+                  <input value={islog} disabled className="disabled-inputy" />
+                </div>
+                <div className="input-container">
+                  <label className="text-white">To Wallet Address</label>
+                  <select
+                    value={destination}
+                    onChange={(e) => setDestination(e.target.value)}
+                    className="inputy"
+                    id="dropdown"
+                    name="options"
+                  >
+                    <option>Select destination</option>
+                    {principals?.map(
+                      (principal) =>
+                        islog !== principal?.principalText && (
+                          <option value={principal?.principal}>
+                            {principal?.principalText?.slice(0, 25) + "..."} -{" "}
+                            {principal?.role}
+                          </option>
+                        )
+                    )}
+                  </select>
+                </div>
+                <div className="input-container">
+                  <label className="text-white">Amount</label>
+                  <input
+                    value={amount}
+                    onChange={(e) => {
+                      let currentValue = e.target.value;
+                      const filteredValue = currentValue
+                        .split("")
+                        .filter((char) => numberStrings.includes(char))
+                        .join("");
+                      setAmount(filteredValue);
+                    }}
+                    className="inputy"
+                  />
+                </div>
+                <div className="input-container">
+                  <label className="text-white">Description</label>
+                  <input
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className="inputy"
+                  />
+                </div>
+                {transactionLoading ? (
+                  <div className="center-all">
+                    <BeatLoader color={"#FFFFFF"} size={30} />
+                  </div>
+                ) : (
+                  <div className="button-container">
+                    <button
+                      onClick={() => setMode("dashboard")}
+                      className="the-button"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      onClick={() => {
+                        if (!destination || !amount || !description) {
+                          return Swal.fire({
+                            title: "Fill the values!",
+                            text: "Please fill in all the inputs!",
+                            icon: "info",
+                          });
+                        }
+                        if (parseInt(amount) === 0) {
+                          return Swal.fire({
+                            title: "Amount info!",
+                            text: "Amount cannot be zero!",
+                            icon: "info",
+                          });
+                        }
+                        setTransactionLoading(true);
+                        IC.getBackend(async (result) => {
+                          const destinationPrincipal = principals?.find(
+                            (thePrincipal) =>
+                              thePrincipal?.principalText === destination
+                          )?.principal;
+                          const finalResult = await result.addTransaction(
+                            currentPrincipal,
+                            destinationPrincipal,
+                            parseInt(amount),
+                            description
+                          );
+                          setAmount("");
+                          setDescription("");
+                          setDestination("");
+                          setTransactionLoading(false);
+                          Swal.fire({
+                            title: "Success!",
+                            text: "Successfully created the transaction!",
+                            icon: "success",
+                          });
+                        });
+                      }}
+                      className="submit-button"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
         </div>
       )}
-      </div>}
       <Modal
         title="Logout"
         open={isModalOpen}
